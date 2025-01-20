17dad6550821f8723d66e4dcae74d8197e31b30c
anaisnz <anaiszaytoun09@gmail.com>
2025-01-20 03:18:04 +0100

    Add files via upload

---
+++ Snake/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Windows;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //où se trouvent les dictionnaires de ressources spécifiques au thème
+                                     //(utilisé si une ressource n'est pas trouvée dans la page ou
+                                     //dans les dictionnaires de ressources de l'application)
+    ResourceDictionaryLocation.SourceAssembly //où se trouve le dictionnaire de ressources générique
+                                              //(utilisé si une ressource n'est pas trouvée dans la page,
+                                              //l'application ou dans les dictionnaires de ressources spécifiques à un thème)
+)]
